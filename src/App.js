5d88b212ed024d6b846777869c2dc9f1c6fd87b6
cham2 <dearcham2@gmail.com>
2023-08-01 16:59:21 +0900

    Feat : Header 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './reset.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,43 @@
+/** @jsxImportSource @emotion/react */
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div css={header}>
+      <img src="/TODO_logo.png" alt="logo" />
+      <div css={headerLinks}>
+        <a css={headerLink} href="/signin">
+          로그인
+        </a>
+        /
+        <a css={headerLink} href="/signup">
+          회원가입
+        </a>
+      </div>
+    </div>
+  );
 }
+
+// css 스타일링
+
+const header = {
+  height: 56,
+  padding: '0 24px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  color: '#00000055',
+  borderBottom: '1px solid #000',
+};
+
+const headerLinks = {
+  display: 'flex',
+  gap: '8px',
+};
+
+const headerLink = {
+  color: '#00000099',
+  '&:hover': {
+    color: '#000',
+    fontWeight: 500,
+  },
+};
