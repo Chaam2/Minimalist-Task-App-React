2a42846bc41ca8b700e22b20f2d8e519453cbd0c
cham2 <dearcham2@gmail.com>
2023-08-01 17:10:09 +0900

    Feat : Signin, Signup 기본 Html 생성

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,3 +1,19 @@
 export default function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <h1>로그인</h1>
+      <form>
+        <label htmlFor="email">이메일 주소*</label>
+        <input type="email" id="email" data-testid="email-input" />
+        <label htmlFor="password">비밀번호*</label>
+        <input type="password" id="password" data-testid="password-input" minLength="8" />
+        <button type="submit" data-testid="signin-button">
+          로그인
+        </button>
+      </form>
+      <div>
+        아직 계정이 없다면? <a href="/signup">회원가입</a>
+      </div>
+    </div>
+  );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,19 @@
 export default function Singup() {
-  return <div>Singup</div>;
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <form>
+        <label htmlFor="email">이메일 주소*</label>
+        <input type="email" id="email" data-testid="email-input" />
+        <label htmlFor="password">비밀번호*</label>
+        <input type="password" id="password" data-testid="password-input" minLength="8" />
+        <button type="submit" data-testid="signup-button">
+          회원가입
+        </button>
+      </form>
+      <div>
+        이미 가입하셨다면? <a href="/signup">로그인</a>
+      </div>
+    </div>
+  );
 }
