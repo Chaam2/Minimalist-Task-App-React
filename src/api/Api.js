38975c181eeb3a02b8f67879c0d739acd731de4a
cham2 <dearcham2@gmail.com>
2023-08-01 17:56:10 +0900

    Feat : api 요청 로직 모듈화

---
+++ src/api/Api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+// axios 인스턴스 생성
+const Api = axios.create({
+  baseURL: 'https://www.pre-onboarding-selection-task.shop',
+});
+
+// 요청 인터셉터 - 헤더 설정
+Api.interceptors.request.use(async (config) => {
+  const Token = localStorage.getItem('Token');
+  config.headers.Authorization = `Bearer ${Token}`;
+  return config;
+});
+
+// 응답 인터셉터
+Api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+
+  async (error) => {
+    alert(error.response.data.message);
+  }
+);
+
+export default Api;
