e3fd3e41aab8f9f0879c69070d7fdbd86736632c
cham2 <dearcham2@gmail.com>
2023-08-01 15:42:15 +0900

    Feat : 기본 페이지 생성 및 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './pages/Main';
+import Signin from './pages/Signin';
+import Singup from './pages/Signup';
+import Todo from './pages/Todo';
+
 function App() {
-  return <div className="App">wanted-pre-onboarding-frontend</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Singup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
