7cdb78fb45e6bc39161280dbb7de31a4fb4740e0
cham2 <dearcham2@gmail.com>
2023-08-02 12:06:17 +0900

    Feat : Todo 페이지 html 생성

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,3 +1,30 @@
 export default function Todo() {
-  return <div>Todo</div>;
+  return (
+    <div>
+      <img
+        width={200}
+        src="https://source.unsplash.com/random/?grayscale"
+        alt="메인 비주얼 이미지"
+      />
+      <ul>
+        <li>To-do list</li>
+        <li>
+          <input type="checkbox" />
+          투두예시
+          <button>✏️</button>
+          <button>🗑️</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          수정모드
+          <button>✅</button>
+          <button>❎</button>
+        </li>
+      </ul>
+      <form>
+        <input type="text" placeholder="+ Add to-do" required />
+        <button>OK</button>
+      </form>
+    </div>
+  );
 }
