9924543ee62c3a67fa9a2e85861e1a57f95d4fc6
cham2 <dearcham2@gmail.com>
2023-08-02 12:50:54 +0900

    Feat : Todo 페이지 read, create로직 구현

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+import { createTodo, getTodo, updateTodo } from '../api/Fetcher';
+
 export default function Todo() {
+  const todoRef = useRef();
+  const [todoList, setTodoList] = useState([]);
+
+  // Create Todo
+  const handleTodoSubmit = async (e) => {
+    e.preventDefault();
+    const todoData = {
+      todo: todoRef.current.value,
+    };
+    await createTodo(todoData);
+    todoRef.current.value = '';
+  };
+
+  const getTodoData = async () => {
+    const todoData = await getTodo();
+    setTodoList(todoData);
+  };
+  // Read Todo
+  useEffect(() => {
+    getTodoData();
+  }, []);
+
   return (
     <div>
       <img
@@ -8,12 +33,24 @@ export default function Todo() {
       />
       <ul>
         <li>To-do list</li>
-        <li>
-          <input type="checkbox" />
-          투두예시
-          <button>✏️</button>
-          <button>🗑️</button>
-        </li>
+        {todoList.map((todo) => {
+          const handleCheckClick = async () => {
+            const checkboxData = {
+              todo: todo.todo,
+              isCompleted: !todo.isCompleted,
+            };
+            await updateTodo(checkboxData, todo.id);
+            await getTodoData();
+          };
+          return (
+            <li key={todo.id}>
+              <input type="checkbox" onChange={handleCheckClick} checked={todo.isCompleted} />
+              <span>{todo.todo}</span>
+              <button>✏️</button>
+              <button>🗑️</button>
+            </li>
+          );
+        })}
         <li>
           <input type="checkbox" />
           수정모드
@@ -21,9 +58,15 @@ export default function Todo() {
           <button>❎</button>
         </li>
       </ul>
-      <form>
-        <input type="text" placeholder="+ Add to-do" required />
-        <button>OK</button>
+      <form onSubmit={handleTodoSubmit}>
+        <input
+          type="text"
+          placeholder="+ Add to-do"
+          ref={todoRef}
+          data-testid="new-todo-input"
+          required
+        />
+        <button data-testid="new-todo-add-button">OK</button>
       </form>
     </div>
   );
